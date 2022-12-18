b5c92535fb738107d01487e0aab9abd730f2bf18
erickssguerra <erickssguerra@gmail.com>
2022-12-18 18:05:33 -0300

    feat: implement postUrlShorten

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,9 +1,21 @@
 import chalk from "chalk";
 import connectionDB from "../database/database.js";
+import { nanoid as shortenUrlCreator } from "nanoid";
 
 export async function postUrlShorten(req, res) {
   const userId = res.locals.authorizedUserId;
   const url = res.locals.schemaValidatedUrl;
-
-  res.status(201).send({ url, userId });
+  const shortUrl = shortenUrlCreator(8);
+  try {
+    await connectionDB.query(
+      `INSERT INTO urls ("userId", url, "shortUrl")
+       VALUES ($1, $2, $3);`,
+      [userId, url, shortUrl]
+    );
+    console.log(chalk.green("C: postUrlShorten concluded!"))
+    res.status(201).send({ shortUrl });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
