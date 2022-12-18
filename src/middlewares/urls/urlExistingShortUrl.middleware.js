6dc0b74ccde454ec9e1ba5bc2ef130656d3cc86d
erickssguerra <erickssguerra@gmail.com>
2022-12-18 19:04:56 -0300

    fix: filtering by id

--- src/middlewares/urls/urlExistingShortUrl.middleware.js
+++ src/middlewares/urls/urlExistingShortUrl.middleware.js
@@ -2,18 +2,17 @@ import chalk from "chalk";
 import connectionDB from "../../database/database.js";
 
 export async function existingShortUrl(req, res, next) {
-  const shortUrl = req.params.id;
+  const id = req.params.id;
   try {
     const existingShortUrl = await connectionDB.query(
-      `SELECT * FROM urls WHERE "shortUrl" = $1;`,
-      [shortUrl]
+      `SELECT * FROM urls WHERE id = $1;`,
+      [id]
     );
     if (!existingShortUrl.rowCount) {
       console.log(chalk.red("MW: existingShortUrl failed!"));
       return res.status(404).send({ message: "Url não encontrada!" });
     } else {
       console.log(chalk.yellow("MW: existingShortUrl passed..."));
-      
       res.locals.existingShortUrl = existingShortUrl.rows[0];
     }
   } catch (err) {
