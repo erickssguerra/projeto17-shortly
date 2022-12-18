da76ccfaca6110b669f5f2bde706307556dab9cf
erickssguerra <erickssguerra@gmail.com>
2022-12-18 13:06:00 -0300

    feat: post SignIn implemented

--- src/controllers/sessions.controllers.js
+++ src/controllers/sessions.controllers.js
@@ -1,7 +1,21 @@
 import chalk from "chalk";
 import connectionDB from "../database/database.js";
+import { nanoid as tokenGenerator } from "nanoid";
 
 export async function postSignIn(req, res) {
   const user = res.locals.authorizedUser;
-  res.status(200).send(user);
+  const token = tokenGenerator(50);
+  try {
+    await connectionDB.query(
+      `
+    INSERT INTO sessions ("userId", token) VALUES ($1, $2)    
+    ;`,
+      [user.id, token]
+    );
+    console.log(chalk.green("C: postSignIn concluded!"));
+    res.status(200).send({ token });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
